bac750b8ddc0e6ef0bb9d914b67fea414320022a
Al-Tubn 🐲 <166261323+dragonSlaye1@users.noreply.github.com>
2024-04-08 23:42:26 +0300

    Add files via upload

---
+++ mody.py
@@ -0,0 +1,19 @@
+import os
+
+
+class Mody(object):
+    Yasr_0 = os.environ.get("Yasr_0", "")
+    
+    OWNER = os.environ.get("OWNER", "5808770835")
+    
+    ID_BOT = int(os.environ.get("ID_BOT", ""))
+    
+    USER_BOT = os.environ.get("USER_BOT", "")
+    
+    MAX_ACCOUNTS = int(os.environ.get("MAX_ACCOUNTS", ""))
+    
+    API_ID = os.environ.get("API_ID", "25281175")
+    
+    API_HASH = os.environ.get("API_HASH", "8892e6466c6799c70d317446453f23f6")
+    
+
